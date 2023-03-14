1bc96bd7ac63e2282caa144509597865e81f34a0
bjkim9763 <bjkim9736@naver.com>
2023-03-14 17:26:34 +0900

    http오류 처리 catch이용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/")
+    setIsLoading(true);
+    setError(null);
+    fetch("https://swapi.dev/api/film/")
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
         return response.json();
-        sdsdsdsd;
       })
       .then((data) => {
         const transformedMovies = data.results.map((movieData) => {
@@ -21,18 +28,41 @@ function App() {
           };
         });
         setMovies(transformedMovies);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
       });
   }; // 이 함수가 호출될 때마다 매 번 http요청이 전송됨
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+  //아래처럼 많은 조건문으로 나타낼수도 있고,
+  //위에 조건을 미리 달아서 content하나로 나타낼 수도 있음
   return (
-    <React.Fragment>
+    <>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {/* {!isLoading && movies.length > 0 && }
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>} */}
+        {content}
       </section>
-    </React.Fragment>
+    </>
   );
 }
 
